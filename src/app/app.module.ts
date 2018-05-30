b3874d459e4a35117cc3172d09a53745879c5a76
bikeredu <luiseduardo.tellez@gmail.com>
2018-05-29 21:48:11 -0500

    primeros pasos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule} from '@angular/http';
 
+// routes
+import { APP_ROUTING } from './app.routes';
+
+// servicios
+import { HeroesService } from './services/heroes.service';
 
 import { AppComponent } from './app.component';
-
-
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroes/heroe.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
